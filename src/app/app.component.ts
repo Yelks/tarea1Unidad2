b0bfe36ec6a403586b4c5d01a6137ba952983995
yelks <yelsin2335@gmail.com>
2022-08-19 20:57:45 -0600

    Unidad 2 Tarea 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tarea1.2';
+  title = 'Calculadora';
+
+  num_1!:number;
+  num_2!:number;
+  resultado!:number;
+
+  suma():void
+  {
+    this.resultado = this.num_1 + this.num_2;
+  }
+
+  resta():void
+  {
+    this.resultado = this.num_1 - this.num_2;
+  }
+
+  multi():void
+  {
+    this.resultado = this.num_1 * this.num_2;
+  }
+
+  division():void
+  {
+    this.resultado = this.num_1 / this.num_2;
+  }
+
 }
